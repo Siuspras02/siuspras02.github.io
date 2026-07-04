934ce072f8fa82304202727b29be31d1d3622e9c
Siusprass <147220023+Siusprass@users.noreply.github.com>
2026-07-04 14:49:39 +0700

    1.4.1
    Nav menu update.
    added version.js

---
+++ version.js
@@ -0,0 +1,10 @@
+// Change this single text string whenever you update your app!
+const CURRENT_VERSION = "v1.4.1";
+
+// Wait for the page to load, then inject the version into any badge it finds
+document.addEventListener("DOMContentLoaded", () => {
+    const versionBadges = document.querySelectorAll(".app-version");
+    versionBadges.forEach(badge => {
+        badge.innerText = CURRENT_VERSION;
+    });
+});

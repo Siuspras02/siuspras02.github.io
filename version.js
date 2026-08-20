4bd2072715a8ebe09b8d90e40b6cc23af78d20d9
Siuspras02 <swifterstar2@gmail.com>
2026-08-20 07:28:36 +0700

    1.5.1
    hotfix

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 // Change this single text string whenever you update your app!
-const CURRENT_VERSION = "v1.4.2";
+const CURRENT_VERSION = "v1.5.1";
 
 // Wait for the page to load, then inject the version into any badge it finds
 document.addEventListener("DOMContentLoaded", () => {
